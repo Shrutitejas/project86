ece29a59b557dd18bca8770bf8a1baadf4aae285
Shrutitejas <96972363+Shrutitejas@users.noreply.github.com>
2022-01-25 20:57:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+fabric.Image.fromURL("BirthdayImage.jpg", function (Img){
+    player=Img;
+    player.scaleToWidth(700);
+    player.scaleToHeight(600);
+    player.set({
+        top:0,
+        left:0
+    });
+    canvas.add(player);
+});	
+	
+}
+
+function playSound(){
+	x.play();
+}
